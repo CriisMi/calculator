3f59725bf6cecb92176e5cdad83042a706d0b2d0
Cris <cristinamihalciuc@yahoo.com>
2022-06-21 22:33:42 +0300

    Change button bg color when clicked

--- script.js
+++ script.js
@@ -15,9 +15,19 @@ const operate = (operator, a, b) => {
 }
 
 const buttons = document.querySelectorAll('button');
+const display = document.querySelector('.display-text');
+
 
 buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        alert('Hello');
+    button.addEventListener('click', function(e) {
+        const originalColor = button.style.backgroundColor;
+        e.target.style.background = '#9b9b9b';
+        setTimeout(function(){
+            e.target.style.backgroundColor = originalColor;  // Change the color back to the original
+          }, 100); 
     });
+    
+});
+buttons.forEach((button) => {
+   
 });
