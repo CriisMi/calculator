97d5d3bfd8d37fa76c3b6ab8530912de0edfe96d
Cris <cristinamihalciuc@yahoo.com>
2022-06-21 23:52:45 +0300

    Add to display keys that were clicked and can clear and backspace

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const sum = (a, b) => {
     return a + b;
 }
@@ -14,17 +15,47 @@ const operate = (operator, a, b) => {
     return operator(a, b);
 }
 
+const clearDisplay = () => {
+    display.textContent = '';
+}
+
+const backspace = () => {
+    display.textContent = display.textContent.slice(0, -1);
+}
+
 const buttons = document.querySelectorAll('button');
 const display = document.querySelector('.display-text');
-
 
 buttons.forEach((button) => {
     button.addEventListener('click', function(e) {
         const originalColor = button.style.backgroundColor;
         e.target.style.background = '#9b9b9b';
         setTimeout(function(){
-            e.target.style.backgroundColor = originalColor;  // Change the color back to the original
-          }, 100); 
+            e.target.style.backgroundColor = originalColor;  
+        }, 100); 
+
+        buttonClicked = button.className;
+        switch (buttonClicked) {
+            case 'clear':
+                clearDisplay();
+                break;
+            case 'backspace':
+                backspace();
+                break;
+            case 'number':
+                display.innerText += e.target.innerText;
+                break;
+            case 'operator':
+                display.innerText += e.target.innerText;
+                break;
+            case 'equal':
+                display.innerText += e.target.innerText;
+                break;
+            case 'point':
+                display.innerText += e.target.innerText;
+                break;
+        }
+
     });
     
 });
